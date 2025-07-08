8c414b46cda66ce15ca63e9ea99bea07aa7a2536
Hussein Ghrer <husseinghrer@mac.speedport.ip>
2025-07-08 08:45:14 +0200

    Initialize project

---
+++ src/main/kotlin/com/ghrer/transaction/TransactionApplication.kt
@@ -0,0 +1,11 @@
+package com.ghrer.transaction
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TransactionApplication
+
+fun main(args: Array<String>) {
+	runApplication<TransactionApplication>(*args)
+}
